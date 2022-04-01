100fd7ce69dd67b568d19a4e4e0581f78be22add
Zita Mello <zitamello@gmail.com>
2022-04-01 10:31:49 -0300

    game complete

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
+//tutorial btn elements
 
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+//plater elements
 const player0Element = document.querySelector('.player--0');
 const player1Element = document.querySelector('.player--1');
 //selecting the ID using #
@@ -13,15 +19,30 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn-new');
 const btnRoll = document.querySelector('.btn-roll');
 const btnHold = document.querySelector('.btn-hold');
+const btnCloseTutorial = document.querySelector('.close-modal');
+const btnTutorial = document.querySelector('.show-modal');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-//starting conditions to 0
-score0Element.textContent = 0;
-score0Element.textContent = 0;
-diceElement.classList.add('hidden');
+//tutorial modal functions
+const openModal = function() {
+    //when using classList, dont use ., just type the name of the class as it is. 
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnTutorial.addEventListener('click', openModal);
+btnCloseTutorial.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(event){
+    if(event.key === "Escape" && !modal.classList.contains('hidden')) {
+        closeModal();
+    }    
+});
 
 //switch player function 
 const switchPlayers = function(){
@@ -32,6 +53,46 @@ const switchPlayers = function(){
     player1Element.classList.toggle('player--active');
 }
 
+//gameOver; 
+
+const gameLost = function(){
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--loser');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    diceElement.classList.add('hidden');
+}
+
+const gameWon = function(){
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner'); 
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    diceElement.classList.add('hidden');
+}
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function(){
+    
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    //starting conditions to 0
+    score0Element.textContent = 0;
+    score1Element.textContent = 0;
+    current0Element.textContent = 0;
+    current1Element.textContent = 0;
+
+    diceElement.classList.add('hidden');
+
+    player0Element.classList.remove('player--winner'); 
+    player0Element.classList.remove('player--loser');
+    player0Element.classList.add('player--active'); 
+    player1Element.classList.remove('player--winner');
+    player1Element.classList.remove('player--loser');
+    player1Element.classList.add('player--active');
+}
+
+init();
 //rolling the dice
 btnRoll.addEventListener('click', function(){
     if(playing){
@@ -62,15 +123,19 @@ btnHold.addEventListener('click', function(){
         //scores[1] = scores[1] + currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-        //2. check if score is at least 100
-        if(scores[activePlayer] >= 20) {
+        //2. check if score is BIGGER than 21
+        if(scores[activePlayer] > 21) {
             playing = false;
-            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-            diceElement.classList.add('hidden');
+            gameLost();
+        } else if (scores[activePlayer] === 21) {
+            playing = false;
+            gameWon();
         } else {
             //4 if not switch to the 2nd player;
             switchPlayers();
         }
     }
 });
+
+
+btnNew.addEventListener('click', init);
